853671ad92352d95202b700f4ecc87008ce51e87
Sebastian <seba78890@gmail.com>
2024-11-27 11:59:13 +0100

    Changes to player and font

---
+++ The Appeal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public GameObject WinScreen;
+    public string sceneName;
+    private Rigidbody2D rb;
+    public float jump = 250;
+    public bool IsGrounded = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        gameObject.transform.Translate(new Vector3(x, 0, 0) * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            rb.AddForce(new Vector2(0, jump));
+            IsGrounded = false;
+            Debug.Log("Jump Triggered: " + IsGrounded);
+        }
+    }
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "DEATH")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        
+        else if (collision.gameObject.tag == "GROUND")
+        {
+            IsGrounded = true;
+            Debug.Log("IsGrounded: " + IsGrounded);
+        }
+    }
+}
