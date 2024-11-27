5b9d1bf3be8278592324c2fbac62b531545b9a2a
Sebastian707 <seba78890@gmail.com>
2024-11-27 21:45:50 +0100

    Boss

---
+++ The Appeal/Assets/Scripts/BossHealthManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BossHealthManager : MonoBehaviour
+{
+    public Image healthbar;
+    public TMP_Text bossNameText;
+    public int healthAmount = 100;
+    public GameObject bossObject;
+
+    void Start()
+    {
+
+        if (bossObject == null)
+        {
+            bossObject = gameObject;
+        }
+
+        if (bossNameText != null)
+        {
+            bossNameText.text = bossObject.name;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) 
+        {
+            TakeDamage(20);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        healthAmount -= damage;
+        healthbar.fillAmount = healthAmount / 100f;
+
+        if (healthAmount <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        if (bossObject != null)
+        {
+            bossObject.SetActive(false);
+        }
+    }
+}
